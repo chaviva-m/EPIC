27761330ad228d8506e3bc01833343f914b6460e
leah <leahorlin@gmail.com>
2019-03-03 19:03:15 +0200

    Reduced self-report emotions

--- ExperimentApp/Infrastructure/Emotions.cs
+++ ExperimentApp/Infrastructure/Emotions.cs
@@ -9,12 +9,8 @@ namespace ExperimentApp.Infrastructure
     {
         public static List<string> SelfReportEmotions { get; set; } = new List<string>
         {
-            "Interested מעונין", "Sad עצוב", "Arousal התרגשות",
-            "Disgust גועל", "Surprised מופתע", "Contentment סיפוק",
-            "Fear פחד", "Amusement בידור", "Confusion בלבול",
-            "Embarrassment מבוכה", "Relief הקלה", "Contempt זלזול",
-            "Pain כאב", "Tension מתיחות", "Happinness שמחה",
-            "Anger כעס"
+            "Apathy אדישות", "Sadness עצב", "Calm רוגע",
+            "Amusement בידור","Pain כאב", "Happinness שמחה",
         };
     }
 }
